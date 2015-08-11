318bad1af49d2ace5650693495217727bbf2db6e
Matthew Ferry <hello@matthew-ferry.com>
2015-08-10 17:49:39 -0700

    less change-centric

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -1,19 +1,10 @@
 var gulp = require('gulp');
-var run = require('gulp-run');
 var concat = require('gulp-concat-css');
 var minify = require('gulp-minify-css');
 
-// Compile CSS and Iconfont from style guide
-gulp.task('styleguide', function() {
-  return gulp.src('node_modules/change-styleguide/')
-    .pipe(run('npm run build', {
-      cwd: 'node_modules/change-styleguide'
-    }));
-});
-
 // Concat styles and move to public dir
-gulp.task('css', ['styleguide'], function() {
-  return gulp.src(['node_modules/change-styleguide/public/boot.css', 'src/styles.css'])
+gulp.task('css', function() {
+  return gulp.src('src/*.css'])
     .pipe(concat('styles.css'))
     .pipe(minify())
     .pipe(gulp.dest('public/'));

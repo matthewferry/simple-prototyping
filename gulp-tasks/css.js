047aeee0ca3a1f42ad1f447667718e54b1f57cae
Matthew Ferry <hello@matthew-ferry.com>
2018-11-02 04:17:13 +0000

    Updates css task to remove accidental bracket

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -4,7 +4,7 @@ var minify = require('gulp-minify-css');
 
 // Concat styles and move to public dir
 gulp.task('css', function() {
-  return gulp.src('src/*.css'])
+  return gulp.src('src/*.css')
     .pipe(concat('styles.css'))
     .pipe(minify())
     .pipe(gulp.dest('public/'));
